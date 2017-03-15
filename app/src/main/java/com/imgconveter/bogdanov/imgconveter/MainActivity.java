9889fe41599d83081d5885337efd185cc44af6f5
Vladislav Bogdanov <vla-bogdano@ya.ru>
2017-03-15 16:47:05 +0300

    create project TEST4

--- app/src/main/java/com/imgconveter/bogdanov/imgconveter/MainActivity.java
+++ app/src/main/java/com/imgconveter/bogdanov/imgconveter/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-// test 3
+// test 4
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
